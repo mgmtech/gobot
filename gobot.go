af5ba6584f93d63409453724e95537a0c5344797
Matt Miller <matt@flashnotes.com>
2013-07-02 12:46:38 -0400

    cp

--- gobot.go
+++ gobot.go
@@ -486,7 +486,7 @@ func main() {
 	cc := IrcChannelLogger{
 		name:   "#flashnotes-dev",
 		host:   "127.0.0.1",
-		port:   6667,
+		port:   6669,
 		nick:   botname,
 		ssl:    false,
 		listen: true,
